cf532c83f5a1a37eee38ce7a30b67a98e5d6fdb4
ignatovallesya <138482796+ignatovallesya@users.noreply.github.com>
2023-07-21 14:20:38 +0600

    Update Pt_2-3.py

--- Pt_2/Pt_2-3.py
+++ Pt_2/Pt_2-3.py
@@ -1,25 +1,14 @@
-#2.3
-def is_armstrong_number(number):
-    # Переводим число в строку для подсчета количества цифр
-    num_str = str(number)
-    # Получаем количество цифр в числе
-    num_digits = len(num_str)
-    # Инициализируем переменную для суммы цифр, возведенных в степень
-    sum_of_digits = 0
-
-    # Проходим по каждой цифре числа
-    for digit in num_str:
-        # Преобразуем цифру обратно в число и возводим в степень
-        sum_of_digits += int(digit) ** num_digits
-
-    # Проверяем, равна ли сумма цифр исходному числу
-    if sum_of_digits == number:
-        return True
-    else:
-        return False
-
-# Получаем число от пользователя
-number = int(input("Введите число: "))
+# 2.3
+num = (int(input("Введите число: ")))
+nums = str(num)
+sum = 0
+n = len(nums)
+for digit in nums:
+    sum += int(digit) ** n
+if num == sum:
+    print(num, "является числом Армстронга")
+else:
+    print(num, "не является числом Армстронга")
 
 # Проверяем, является ли число числом Армстронга
 if is_armstrong_number(number):
