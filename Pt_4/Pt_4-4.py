72f1047ab930028fa92215a47b3c67866cf8b079
ignatovallesya <138482796+ignatovallesya@users.noreply.github.com>
2023-07-21 15:21:24 +0600

    Update Pt_4-4.py

--- Pt_4/Pt_4-4.py
+++ Pt_4/Pt_4-4.py
@@ -1,39 +1,17 @@
-#4.4
-# Задаем список чисел
-numbers = [1, 2, 3, 4, 5]
+# 4.4
+from itertools import combinations
 
-# Задаем целевую сумму
-target_sum = 7
+numbs = [1, 3, 4, 7, 8, 2]
+a = int(input("Введите число: "))
+uniq_com = []
+sum_com = []
 
-# Создаем функцию для поиска комбинаций чисел
-def find_combinations(numbers, target_sum):
-    # Создаем пустой список для хранения комбинаций
-    combinations = []
+for r in range(1, len(numbs) + 1):
+    combs = list(map(list, combinations(numbs, r)))
+    uniq_com.extend(combs)
 
-    # Создаем вспомогательную функцию для рекурсивного поиска комбинаций
-    def find_combinations_recursive(numbers, target_sum, current_sum, current_combination, start_index):
-        # Если текущая сумма равна целевой сумме, добавляем текущую комбинацию в список
-        if current_sum == target_sum:
-            combinations.append(current_combination)
+for combination in uniq_com:
+    if sum(combination) == a:
+        sum_com.append(combination)
 
-        # Если текущая сумма больше целевой суммы, прекращаем поиск
-        if current_sum > target_sum:
-            return
-
-        # Используем цикл для перебора чисел, начиная с заданного индекса
-        for i in range(start_index, len(numbers)):
-            # Добавляем текущее число к текущей сумме
-            new_sum = current_sum + numbers[i]
-            # Создаем новую комбинацию, добавляя текущее число
-            new_combination = current_combination + [numbers[i]]
-            # Рекурсивно вызываем функцию для поиска комбинаций
-            find_combinations_recursive(numbers, target_sum, new_sum, new_combination, i)
-
-    # Вызываем вспомогательную функцию для начала поиска комбинаций
-    find_combinations_recursive(numbers, target_sum, 0, [], 0)
-
-    # Возвращаем список всех найденных комбинаций
-    return combinations
-
-# Вызываем функцию и выводим результаты на экран
-print(find_combinations(numbers, target_sum))
+print(f'Подходящие коомбинации: {sum_com}')
