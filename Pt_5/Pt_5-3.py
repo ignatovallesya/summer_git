5afcc957a0bf756e4f19826e8dd88f4c6b3ca1a8
ignatovallesya <138482796+ignatovallesya@users.noreply.github.com>
2023-07-21 15:14:17 +0600

    Update Pt_5-3.py

--- Pt_5/Pt_5-3.py
+++ Pt_5/Pt_5-3.py
@@ -1,39 +1,24 @@
 import csv
 
 
-# Загрузка данных из файла books.csv
-def load_books():
-    books = []
-    with open('books.csv', 'r') as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            books.append(row)
-    return books
+year_start = int(input("Введите начальный год: "))
+year_end = int(input("Введите конечный год: "))
+# Проверка на правильность введенный значений
+if year_end < year_start:
+    print("Конечный год должен быть больше начального!")
+    exit()
+filename = "prog_books.csv"
 
+with open(filename, "r", encoding="utf-8") as f:
+    reader = csv.DictReader(f)
 
-# Поиск книг в заданном промежутке времени
-def search_books(books, start_year, end_year):
-    found_books = []
-    for book in books:
-        year = int(book['Year'])
-        if start_year <= year <= end_year:
-            found_books.append(book)
-
-    # Вывод результатов поиска
-    if len(found_books) == 0:
-        print("Книги в заданном промежутке времени не найдены.")
-    else:
-        print("Найдены книги в промежутке времени", start_year, "-", end_year)
-        for book in found_books:
-            print("Наз:", book['Title'], "Авт:", book['Author'], "Год:", book['Year'])
-
-
-# Загрузка данных из файла books.csv
-books = load_books()
-
-# Запрос начального и конечного года у пользователя
-start_year = int(input("Введите начальный год: "))
-end_year = int(input("Введите конечный год: "))
-
-# Поиск книг в заданном промежутке времени
-search_books(books, start_year, end_year)
+    # Для каждой записи проверяем,
+    # находится ли год выпуска в промежутке от начального до конечного года
+    found = False
+    for row in reader:
+        year = int(row["Год выпуска"])
+        if year_start <= year <= year_end:
+            found = True
+            print(row["Книга"], row["Год выпуска"], sep=", ")
+    if not found:
+        print("Книги за указанный период не найдены.")
