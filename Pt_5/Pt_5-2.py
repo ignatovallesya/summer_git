060dd2bf34144dff34cd67f7f816954d871e2ebc
ignatovallesya <138482796+ignatovallesya@users.noreply.github.com>
2023-07-21 15:01:34 +0600

    Update Pt_5-2.py

--- Pt_5/Pt_5-2.py
+++ Pt_5/Pt_5-2.py
@@ -1,47 +1,23 @@
-#5.2
+# 5.2
 import csv
 
-# Записи книг в список
-def add_books(books_count):
-    books = []
-    for _ in range(books_count):
-        title = input("Введите название книги: ")
-        author = input("Введите автора книги: ")
-        year = input("Введите год издания книги: ")
-        books.append({'Title': title, 'Author': author, 'Year': year})
+file = open ("books.csv", "a")
+num = int(input("Сколько записей вы хотите добавить в список: "))
+for i in range(num):
+    book = input("Введите название книги: ")
+    author = input("Введите автора: ")
+    year_release = input("Введите год издания: ")
+    new_record = book + ", " + author + ", " + year_release + "\n"
+    file.write(str(new_record))
+file.close()
 
-    # Записываем данные в файл books.csv
-    with open('books.csv', 'w', newline='') as file:
-        writer = csv.DictWriter(file, fieldnames=['Title', 'Author', 'Year'])
-        writer.writeheader()
-        writer.writerows(books)
-
-    return books
-
-# Поиск книг по автору
-def search_books(author):
-    with open('books.csv', 'r') as file:
-        reader = csv.DictReader(file)
-        found_books = []
-        for row in reader:
-            if row['Author'] == author:
-                found_books.append(row)
-
-        if len(found_books) == 0:
-            print("Книги этого автора не найдены.")
-        else:
-            print("Найдены книги автора", author)
-            for book in found_books:
-                print(book)
-
-# Запрашиваем количество книг, которые пользователь хочет добавить
-books_count = int(input("Сколько книг вы хотите добавить? "))
-
-# Добавляем книги и получаем список книг
-books = add_books(books_count)
-
-# Запрашиваем автора для поиска
-author = input("Введите автора, чтобы найти его книги: ")
-
-# Ищем книги указанного автора
-search_books(author)
+file = open("books.csv", "r")
+find_author = input("Введите автора для поиска: ")
+count = 0
+for row in file:
+ if find_author in str(row):
+ count += 1
+ print(row)
+if count == 0:
+ print ("В списке нет ни одной книги этого автора")
+file.close()
