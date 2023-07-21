1dd2cad7b7bdca0bacb6a4842e51dd5779cca2e0
ignatovallesya <138482796+ignatovallesya@users.noreply.github.com>
2023-07-21 14:00:15 +0600

    Update Pt_1.7.py

--- Pt_1/Pt_1.7.py
+++ Pt_1/Pt_1.7.py
@@ -1,17 +1,3 @@
-# 1.7
-def toggle_case(input_string):
-    toggled_string = ""
-    for char in input_string:
-        if char.islower():
-            toggled_string += char.upper()
-        elif char.isupper():
-            toggled_string += char.lower()
-        else:
-            toggled_string += char
-    return toggled_string
-
-input_string = input("Введите строку: ")
-
-result = toggle_case(input_string)
-
-print("Результат:", result)
+text = input("Введите текст: ")
+new_text = text.swapcase()
+print(new_text)
