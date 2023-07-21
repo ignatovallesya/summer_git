122be1a0ee7da9093621f6d3668c174266a3a313
ignatovallesya <138482796+ignatovallesya@users.noreply.github.com>
2023-07-21 14:49:36 +0600

    Update Pt_3-7.py

--- Pt_3/Pt_3-7.py
+++ Pt_3/Pt_3-7.py
@@ -1,23 +1,7 @@
-#3.7
-# Функция для проверки, является ли буква гласной
-def is_vowel(letter):
-    vowels = "aeioyuAEIOYU"
-    return letter in vowels
-
-# Вводим строку
-string = input("Введите строку: ")
-
-# Создаем пустой словарь
-letter_dict = {}
-
-# Используем цикл for для прохода по каждой букве в строке
-for letter in string:
-    # Вызываем функцию is_vowel для проверки, является ли буква гласной
-    is_vowel_letter = is_vowel(letter)
-    # Добавляем пару ключ-значение в словарь
-    letter_dict[letter] = is_vowel_letter
-
-# Выводим словарь на экран
-print("Словарь с гласными и согласными буквами:")
-for key, value in letter_dict.items():
-    print(key, ":", value)
+# 3.7
+vowels = 'уеёыаоэяию'
+consonants = 'йцкнгшщзхъфвпрлджчсмтьб'
+s = input("Введите строку: ")
+letter_dict = {letter: True if letter.lower() in vowels
+               else False for letter in s if letter.isalpha()}
+print(letter_dict)
