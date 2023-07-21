ce171b9d3b1baeebc99d586c6dd5e35e68b7c5c3
ignatovallesya <138482796+ignatovallesya@users.noreply.github.com>
2023-07-21 14:25:55 +0600

    Update Pt_2-6.py

--- Pt_2/Pt_2-6.py
+++ Pt_2/Pt_2-6.py
@@ -1,9 +1,9 @@
-#2.6
+# 2.6
 distance = float(input("Сколько километров хотите проехать на автомобиле? "))
-fuel_consumption = float(input("Сколько литров топлива расходует автомобиль на 100 километров? "))
+fuel_consumption = float(input("Сколько литров расходует на 100 километров? "))
 fuel_in_tank = float(input("Сколько литров топлива в вашем баке? "))
 
-fuel_needed = (distance / 100) * fuel_consumption  # Расчет необходимого количества топлива
+fuel_needed = (distance / 100) * fuel_consumption
 
 if fuel_in_tank >= fuel_needed:
     print("Вы сможете проехать желаемое расстояние.")
